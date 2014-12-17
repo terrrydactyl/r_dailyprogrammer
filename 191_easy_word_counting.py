1cff475e33c428b74c3fbe5062b58bf059a79265
Teresa <tcho708@gmail.com>
2014-12-17 14:50:53 -0800

    Challange #191

---
+++ 191_easy_word_counting.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# [2014-12-1] Challenge #191 [Easy] Word Counting
+
+import string
+from collections import Counter
+def strip_beg_end_nonalpha(word):
+    while word and (not word[0].isalpha() or not word[-1].isalpha()):
+        if not word[0].isalpha():
+            word = word[1:]
+        elif not word[-1].isalpha():
+            word = word[:-1]
+    return word
+        
+def book_word_count(book_url):
+    with open(book_url) as f:
+        data = f.read()
+    punctuation = set(string.punctuation)
+    data = data.split()
+    words = []
+    for word in data:
+        if word not in punctuation:
+            word = strip_beg_end_nonalpha(word)
+            if word:
+                words.append(word.lower())
+    count = Counter()
+    for word in words:
+        count[word] += 1
+    return count
